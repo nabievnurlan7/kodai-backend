4ab05113774545835eea00e296bca1938b1a2629
n.nabiyev <n.nabiyev@vlife.kz>
2020-07-05 15:33:11 +0600

    [ADDED] CrossDao table fixes

--- src/di/CrossDao.kt
+++ src/di/CrossDao.kt
@@ -2,7 +2,7 @@ package com.futuris.di
 
 import org.jetbrains.exposed.sql.Table
 
-object CrossDao : Table("cross") {
+object CrossDao : Table("crossing") {
     val tagId = CrossDao.integer("tag_id").primaryKey()
     val questionId = CrossDao.integer("question_id")
 }
